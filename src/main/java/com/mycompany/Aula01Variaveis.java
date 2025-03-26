3378b00d45a212cbfaa2551827ebf7a528e6f070
Murilo Soares Dias <murilo.soares.dias@escola.pr.gov.br>
2025-03-26 19:40:51 +0000

    utilizando print

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,21 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+
+    public static void main(String[] args) {
+        // Variável lógica
+
+        // Tipo primitívo
+        boolean valorLogicoPrimitivo = true;
+
+        // Exibe o valor
+        System.out.println(valorLogicoPrimitivo);
+
+
+        // Tipo abstrato
+        boolean valorlogicoabstrato = false; 
+        // Exibe com texto
+        System.out.println("O valor a variavel valorLogicoAbstrato é: " + valorLogicoAbstrato);
+    
+    } 
+}
