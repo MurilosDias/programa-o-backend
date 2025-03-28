4a4344bd62c019756da8634a4e393a127cb1f017
Murilo Soares Dias <murilo.soares.dias@escola.pr.gov.br>
2025-03-28 13:49:46 +0000

    aula02devariavel

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -17,5 +17,31 @@ public class Aula01Variaveis {
         // Exibe com texto
         System.out.println("O valor a variavel valorLogicoAbstrato é: " + valorLogicoAbstrato);
     
-    } 
+      // Variáveis inteira
+
+       // tipos primitivos
+       int valorInteiro = 10; //32 bits
+       long valorInteiroLongo = 10l; // 64bits 
+
+       // Tipos abstrato
+       Integer ValorInteiroABs = 20;
+       Long valorLongoABs = 20;
+
+       System.out.ptint('o valorInteiro é: " + , e o valor de valorinteiroLongo é: + valorInteiroLongo);
+     
+       // Variáveis Decimais
+
+       // tipos primitivos
+        float numeroFloat = 10.5f; // 32 bits
+        double numeroDouble = 10.5f; // 64 bits
+
+       // Tipos abstrato
+        float numeroFloatABs = 10.5f; 
+        double numeroDoubleABs=10.5f; 
+
+       System.out.ptint('o valorInteiro é: " + , e o valor de valorinteiroLongo é: + valorInteiroLongo);
+
+       
+    }
+
 }
